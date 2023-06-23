ddc39963be839cd44049453c8f33aec69e99aafc
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2023-06-23 09:55:07 +0800

    Added Java Challenges and Workbooks. Completed Module 1.1

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("BBBB   RRRR   BBBB ");
+         System.out.println("B   B  R   R  B   B");
+         System.out.println("B   B  R   R  B   B");
+         System.out.println("BBBB   RRRR   BBBB ");
+         System.out.println("B   B  R R    B   B");
+         System.out.println("B   B  R  R   B   B");
+         System.out.println("BBBB   R   R  BBBB ");
+    }
+}
