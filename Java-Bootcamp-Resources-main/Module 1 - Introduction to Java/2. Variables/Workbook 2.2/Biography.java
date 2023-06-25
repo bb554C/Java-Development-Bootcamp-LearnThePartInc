c82ac7804fbf2e1c2f28426b860d5d851d2f3d8c
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2023-06-25 21:18:21 +0800

    Module 1.2 Variables

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -5,18 +5,26 @@ public class Biography {
 
 
         // name (text)
+        String name = "Baird";
         // age (whole number)
+        int age = 25;
         // country (text)
+        String country = "Philippines";
         // sport (text)
+        String sport = "chess";
         // hours (whole number)
+        int hours = 5;
         // game (text)
+        String game = "chill games";
         // subject (text)
+        String subject = "Science";
         // grade (character)
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day.");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
 
         // Compare your result to what's on Learn the Part.
     }
